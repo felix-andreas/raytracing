19a0cf2f904103cbb17e1615e00f7381e8596398
Felix Andreas <felix.andreas95@googlemail.com>
2023-09-05 23:25:38 +0200

    refactor: Make `DVec3` a struct with named fields

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,21 @@ fn main() {
     let height = 720;
     let pixels = (0..width)
         .cartesian_product(0..height)
-        .map(|(i, j)| {
-            DVec3(
-                i as f64 / (width - 1) as f64,
-                j as f64 / (height - 1) as f64,
-                0.0,
-            )
+        .map(|(i, j)| DVec3 {
+            x: i as f64 / (width - 1) as f64,
+            y: j as f64 / (height - 1) as f64,
+            z: 0.0,
         })
         .collect::<Vec<_>>();
     write_ppm(width, height, &pixels);
 }
 
-struct DVec3(f64, f64, f64);
+struct DVec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
 struct Ray {
     origin: DVec3,
     direction: DVec3,
@@ -24,11 +27,11 @@ struct Ray {
 
 impl Ray {
     fn at(&self, t: f64) -> DVec3 {
-        DVec3(
-            self.origin.0 + t * self.direction.0,
-            self.origin.1 + t * self.direction.1,
-            self.origin.2 + t * self.direction.2,
-        )
+        DVec3 {
+            x: self.origin.x + t * self.direction.x,
+            y: self.origin.y + t * self.direction.y,
+            z: self.origin.z + t * self.direction.z,
+        }
     }
 }
 
@@ -39,7 +42,12 @@ fn write_ppm(width: i32, height: i32, pixels: &[DVec3]) {
             "P3\n{width} {height}\n255\n{}",
             pixels
                 .iter()
-                .map(|DVec3(r, g, b)| format!("{:.0} {:.0} {:.0}", 255.0 * r, 255.0 * g, 255.0 * b))
+                .map(|DVec3 { x: r, y: g, z: b }| format!(
+                    "{:.0} {:.0} {:.0}",
+                    255.0 * r,
+                    255.0 * g,
+                    255.0 * b
+                ))
                 .join("\n")
         ),
     )
