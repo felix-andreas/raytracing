f06101b984d08aa05b03b97ae67d14a0ed52d228
Felix Andreas <felix.andreas95@googlemail.com>
2023-09-05 23:23:29 +0200

    feat: Implement new `Ray` struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use itertools::Itertools;
 
 fn main() {
-    let width = 256;
-    let height = 256;
+    let width = 1280;
+    let height = 720;
     let pixels = (0..width)
         .cartesian_product(0..height)
         .map(|(i, j)| {
-            Pixel(
+            DVec3(
                 i as f64 / (width - 1) as f64,
                 j as f64 / (height - 1) as f64,
                 0.0,
@@ -16,16 +16,30 @@ fn main() {
     write_ppm(width, height, &pixels);
 }
 
-struct Pixel(f64, f64, f64);
+struct DVec3(f64, f64, f64);
+struct Ray {
+    origin: DVec3,
+    direction: DVec3,
+}
 
-fn write_ppm(width: i32, height: i32, pixels: &[Pixel]) {
+impl Ray {
+    fn at(&self, t: f64) -> DVec3 {
+        DVec3(
+            self.origin.0 + t * self.direction.0,
+            self.origin.1 + t * self.direction.1,
+            self.origin.2 + t * self.direction.2,
+        )
+    }
+}
+
+fn write_ppm(width: i32, height: i32, pixels: &[DVec3]) {
     std::fs::write(
         "out.ppm",
         format!(
             "P3\n{width} {height}\n255\n{}",
             pixels
                 .iter()
-                .map(|Pixel(r, g, b)| format!("{:.0} {:.0} {:.0}", 255.0 * r, 255.0 * g, 255.0 * b))
+                .map(|DVec3(r, g, b)| format!("{:.0} {:.0} {:.0}", 255.0 * r, 255.0 * g, 255.0 * b))
                 .join("\n")
         ),
     )
